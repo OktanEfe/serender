ff85ea22c601846a712e23697d6cd55b5a8f27ba
OktanEfe <oktanefe1589@outlook.com>
2026-05-01 01:18:51 +0300

    fix route.ts

--- src/components/contact/ContactPage.tsx
+++ src/components/contact/ContactPage.tsx
@@ -28,7 +28,7 @@ const ContactPage = () => {
     setError("");
 
     try {
-      const res = await fetch("/api/contact", {
+      const res = await fetch("/api/send", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
